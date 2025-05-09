4238c70c73c749692e3088661fb616f7d8034114
mehavish-tamboli <mehavishtamboli@gmail.com>
2025-05-09 23:30:24 +0530

    forcommit

---
+++ 4-Assi.js
@@ -0,0 +1,22 @@
+function stringBasics(){
+
+var dreamCompany="xyz";
+console.log(`My dream company is: ${dreamCompany}`);
+
+
+// 2.1 Log all hobbies on the same line
+var Hobby1="cooking";
+var hobby2="Travelling";
+var hobby3="playing and learn new things";
+console.log(`My Hobbies are:  ${Hobby1}, ${hobby2}, ${ hobby3}`);
+
+ // 2.2 Log total number of characters in all hobbies
+  var totalCharacters = Hobby1.length + hobby2.length + hobby3.length;
+  console.log(`Sum of the total number of character: ${totalCharacters}`);
+}
+
+
+
+
+
+stringBasics();
