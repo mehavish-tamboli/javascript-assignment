19f149d4c406f2cb397a6fb74f384d3bfe90b714
mehavish-tamboli <mehavishtamboli@gmail.com>
2025-05-09 21:57:01 +0530

    first commit

---
+++ 1Assi.js
@@ -0,0 +1,37 @@
+console.log("=========== Variable ===========");
+
+// Variable declaration without initialization
+var fullName;
+console.log("Before initialization My Full Name is:", fullName);
+
+// Variable initialization
+fullName = "Mehvish Tamboli";
+console.log("After initialization My Full Name is:", fullName);
+
+// Updating variable value
+console.log("========Update variable Value========");
+fullName = "Mehvish Rafik Tamboli";
+console.log("My Full Name is:", fullName);
+
+// Declare and initialize variable in one line
+console.log("=======Declare and initialize variable in one line=======");
+var love = "I Love Only JS";
+console.log(love);
+
+// Declare and initialize multiple variables
+console.log("=======Declare and initializing variables========");
+var firstName = "Mehvish";
+var lastName = "Tamboli";
+var age = 21;
+var address = "A.nagar";
+var pinCode = 413704;
+var gender = "Female";
+var isMarried = false;
+
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("Age:", age);
+console.log("Address:", address);
+console.log("PinCode:", pinCode);
+console.log("Gender", gender);
+console.log("IsMarried:", isMarried);
