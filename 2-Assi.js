3103a86e0e64f16e7e06724f395d8b265fcb152f
mehavish-tamboli <mehavishtamboli@gmail.com>
2025-05-09 21:58:05 +0530

    Add files via upload

---
+++ 2-Assi.js
@@ -0,0 +1,10 @@
+console.log("=========Details for bank========");
+
+function bankDetails(bankName, accountNum, location, pinCode) {
+    console.log(`Bank Name: ${bankName}   Account No: ${accountNum}   Location: ${location}   Pin Code: ${pinCode}`);
+}
+
+bankDetails("SBI Bank",64920816802, "Pune", 431202);
+bankDetails("Central Bank",2390148903, "Nagar", 413704);
+bankDetails("Axis Bank", 230462892, "Pune", 411202);
+
