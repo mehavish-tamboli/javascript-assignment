7f4739a7abcfa4239c4003715cf07e7deb46b4f5
mehavish-tamboli <mehavishtamboli@gmail.com>
2025-05-09 22:02:29 +0530

    three assi

---
+++ 3-Assi.js
@@ -0,0 +1,32 @@
+console.log("============== Function with no arguments and no return type===============================");
+console.log("Hi");
+console.log("hello");
+
+console.log("============ Function for personalDetails==========================");
+function personalDetails(firstName,lastName,collegeName){
+console.log("First Name:"+firstName);
+console.log("Large Name:"+lastName);
+console.log("college name:"+collegeName);
+
+}
+
+// Calling the function
+personalDetails("Mehvish", "Tamboli", "RKMM");
+
+
+console.log("======= function with three parameter=================");
+function addThreeValues(a, b, c) {
+    let result = a + b + c;
+    console.log("Addition is: " + result);
+}
+
+// Calling the function with numeric values
+addThreeValues(10.23, 600, 40); 
+
+addThreeValues("Hello", "good", "Morning");  
+
+
+
+
+
+
