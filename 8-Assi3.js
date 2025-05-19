06beff689e622a6701c2527859be06aeb3a510f3
mehavish-tamboli <mehavishtamboli@gmail.com>
2025-05-19 17:51:35 +0530

    Add files via upload
    initial commit

---
+++ 8-Assi3.js
@@ -0,0 +1,74 @@
+console.log("=====Switch Case Month==========================");
+function monthOfYear(Month){
+    switch (Month) {
+        case 1:
+            console.log("January");
+            
+        break;
+        case 2 :
+            console.log("february");
+            break;
+
+            case 3:
+                console.log("march");
+                break;
+
+                case 4:
+                    console.log("April");
+                    break;
+                    case 5:
+                        console.log("the number is 5: May");
+                        break;
+                        case 6:
+                            console.log("June");
+                            break;
+
+                             case 7:
+                            console.log("July");
+                            break;
+
+                             case 8:
+                            console.log("August");
+                            break;
+
+                             case 9:
+                            console.log("September");
+                            break;
+
+                             case 10:
+                            console.log("October");
+                            break;
+
+                             case 11:
+                            console.log("November");
+                            break;
+
+                             case 12:
+                            console.log("December");
+                            break;
+                            
+                        
+                    
+    
+        default:
+            console.log("Invalid month number");
+            
+            break;
+    }
+
+}
+  monthOfYear(0);
+  monthOfYear(5);
+
+   monthOfYear(12);
+   monthOfYear(100);
+
+    monthOfYear(2);
+
+    monthOfYear(15);
+
+      
+
+
+
+     
