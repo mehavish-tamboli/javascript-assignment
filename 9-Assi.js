f3eb2f8cb770fd6923496ad57d09748608ba0772
mehavish-tamboli <mehavishtamboli@gmail.com>
2025-05-19 17:52:20 +0530

    initial commit
    initial commit

---
+++ 9-Assi.js
@@ -0,0 +1,59 @@
+
+console.log("===Print the number 5 to 15========================");
+
+for (let index = 5; index <=15; index++){
+console.log(index);
+   
+}
+
+console.log("====print the number of 50 to 40===================");
+
+
+for (let index = 50; index >=40; index--) {
+console.log(index);
+    
+}
+
+
+console.log("===Print the first 15 ODD numbers=======================");
+for (let index = 1; index <= 30; index++) {
+if (index%2!=0) {
+    console.log(index);
+    
+    
+}    
+}
+
+
+console.log("========print first 10 even numbers==========");
+for (let index = 1; index <=20; index++) {
+if (index%2 ==0) {
+    console.log(index);
+    
+    
+}    
+}
+
+
+console.log("====print  the table of 5===========");
+for (let index = 5; index <=50; index=index+5) {
+console.log(index);
+    
+}
+
+console.log("===print the table of 10============");
+for (let index = 10; index <=100; index=index+10) {
+console.log(index);
+    
+}
+
+console.log("===print the table of 10 in reverse order================");
+for (let index = 10; index >=1; index--) {
+
+console.log(index*10);
+    
+}
+
+
+
+
